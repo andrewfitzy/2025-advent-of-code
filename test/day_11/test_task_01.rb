035074ca9d5cff151b30576f4647efa2be1636ed
Andrew Worden-Fitzpatrick <andrewpfitzpatrick@gmail.com>
2025-12-16 17:48:17 +0000

    day 11 skeleton

---
+++ test/day_11/test_task_01.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative '../../lib/day_11/task_01'
+require_relative '../test_utils/get_input'
+require_relative '../test_utils/skip_ci'
+
+class Day11Task01Test < Minitest::Test
+  include Day11Task01
+  include TestUtils
+
+  def test_solve_example_01
+    # Given
+    input_path = File.join(__dir__, 'example_input_01.txt')
+    puzzle_input = get_input(input_path)
+
+    # When
+    result = solve(data: puzzle_input)
+
+    # Then
+    assert_equal(10, result)
+  end
+
+  def test_solve_puzzle
+    skip_on_ci
+    # Given
+    input_path = File.join(__dir__, 'input.txt')
+    puzzle_input = get_input(input_path)
+
+    # When
+    result = solve(data: puzzle_input)
+
+    # Then
+    assert_equal(589, result)
+  end
+end
